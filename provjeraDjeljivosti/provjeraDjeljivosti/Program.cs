95b0a0aca535c20ae199d85959e336e9c771cff8
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-13 13:36:11 +0200

    Add files via upload

---
+++ provjeraDjeljivosti/provjeraDjeljivosti/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite broj: ");
+        int broj = Convert.ToInt32(Console.ReadLine());
+
+        if (broj % 4 == 0 && broj % 6 == 0)
+        {
+            Console.WriteLine("Broj je djeljiv sa 4 i sa 6");
+        }
+        else if (broj % 4 == 0)
+        {
+            Console.WriteLine("Broj je djeljiv s 4");
+        }
+        else if (broj % 6 == 0)
+        {
+            Console.WriteLine("Broj je djeljiv s 6");
+        }
+        else
+        {
+            Console.WriteLine("Broj nije djeljiv sa 4 ni sa 6");
+        }
+    }
+}
+
